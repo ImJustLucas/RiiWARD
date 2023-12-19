b7726f78d98f98bb73b9b575a1d748f170834be6
Lucas <bellierlucas.pro@gmail.com>
2023-12-19 21:49:42 +0100

    🔧 add config for supabase

---
+++ src/services/Supabase/index.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+export const supabase = createClient(
+  supabaseUrl as string | "",
+  supabaseKey as string | "",
+);
