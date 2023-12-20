60bd4f7419b3b98b0c6c4f2b338bb8c1c3388081
Lucas Huerta <huertalucas13@gmail.com>
2023-12-20 11:49:36 +0100

    [EDIT] Link to home & profil && start section project profil

--- src/screens/Profil.tsx
+++ src/screens/Profil.tsx
@@ -1,42 +1,70 @@
-// Import de la composante Image depuis "next/image"
+import { LeftButton } from "@components/Common/Buttons/LeftButton";
 import Image from "next/image";
 import styled from "styled-components";
 
 export const ProfileScreen: React.FC = () => {
   return (
     <>
-      <LoginContainer>
-        <Title>My profil</Title>
-        <InfosUserContainer>
-          <RowInfosUser>
-            {/* Utilisation de la composante Image de next/image */}
-            <Image width={200} height={200} src={""} alt="avatar" />
+      <ContainerPageProfil>
+        <ContainerButton>
+          <LeftButton text="Back to home" icon="skip-left" link="/" />
+        </ContainerButton>
+        <ProfilContainer>
+          <Title>My profil</Title>
+          <InfosUserContainer>
+            <RowInfosUser>
+              {/* Utilisation de la composante Image de next/image */}
+              <Image width={200} height={200} src={""} alt="avatar" />
+              <InfosUser>
+                <p>
+                  <input type="text" placeholder="Prénom" />
+                </p>
+                <p>
+                  <input type="text" placeholder="Nom" />
+                </p>
+              </InfosUser>
+            </RowInfosUser>
             <InfosUser>
-              <p>
-                <input type="text" placeholder="Prénom" />
-              </p>
-              <p>
-                <input type="text" placeholder="Nom" />
-              </p>
+              <p>description</p>
+              <p>Axe</p>
             </InfosUser>
-          </RowInfosUser>
-          <InfosUser>
-            <p>description</p>
-            <p>Axe</p>
-          </InfosUser>
-        </InfosUserContainer>
-      </LoginContainer>
+          </InfosUserContainer>
+        </ProfilContainer>
+        <ContainerProjectProfil>
+          <Title>My projects</Title>
+        </ContainerProjectProfil>
+      </ContainerPageProfil>
     </>
   );
 };
 
-const LoginContainer = styled.div`
+const ProfilContainer = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 50px;
   padding: 50px 50px;
+`;
+
+const ContainerPageProfil = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const ContainerButton = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: max-content;
+`;
+
+const ContainerProjectProfil = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 `;
 
 const InfosUserContainer = styled.div`
@@ -69,7 +97,7 @@ const Title = styled.h1`
   font-size: ${({ theme }) => theme.size.desktop.title};
   text-align: center;
   font-weight: 700;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.dark};
   position: relative;
   z-index: 2;
 
