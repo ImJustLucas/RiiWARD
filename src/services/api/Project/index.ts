e89ebbde010fa76d4fcfdea27fe3e7a4ca7078f3
Lucas <bellierlucas.pro@gmail.com>
2024-02-12 16:00:57 +0100

    👔 add method to get project by id

--- src/services/api/Project/index.ts
+++ src/services/api/Project/index.ts
@@ -2,18 +2,6 @@ import { supabase } from "@services/Supabase";
 import { PostgrestError } from "@supabase/supabase-js";
 import { Project, ProjectData } from "@typesDef/project/project";
 
-export async function createProject(projet: Project) {
-  try {
-    const { data: aw_projects, error } = await supabase
-      .from("aw_projects")
-      .insert([projet]);
-    return { aw_projects, error };
-  } catch (error) {
-    console.error("Error:", error);
-    return null;
-  }
-}
-
 export class ProjectsServices {
   async getProjects() {
     try {
@@ -24,22 +12,47 @@ export class ProjectsServices {
       return null;
     }
   }
-}
 
-export async function getAllUserProject(userId: string): Promise<ProjectData> {
-  try {
-    const { data: projects, error } = await supabase
-      .from("aw_projects")
-      .select("*")
-      .eq("userId", userId);
+  async getAllUserProject(userId: string): Promise<ProjectData> {
+    try {
+      const { data: projects, error } = await supabase
+        .from("aw_projects")
+        .select("*")
+        .eq("userId", userId);
 
-    return { project: projects, error };
-  } catch (error: unknown) {
-    const postgrestError = error as PostgrestError | null;
-    console.error(
-      "Erreur lors de la récupération des projets :",
-      postgrestError,
-    );
-    return { project: null, error: postgrestError };
+      return { project: projects, error };
+    } catch (error: unknown) {
+      const postgrestError = error as PostgrestError | null;
+      console.error(
+        "Erreur lors de la récupération des projets :",
+        postgrestError,
+      );
+      return { project: null, error: postgrestError };
+    }
+  }
+
+  async createProject(projet: Project) {
+    try {
+      const { data: aw_projects, error } = await supabase
+        .from("aw_projects")
+        .insert([projet]);
+      return { aw_projects, error };
+    } catch (error) {
+      console.error("Error:", error);
+      return null;
+    }
+  }
+
+  async getProjectById(projectId: string) {
+    try {
+      const { data: project, error } = await supabase
+        .from("aw_projects")
+        .select("*")
+        .eq("id", projectId);
+      return { project, error };
+    } catch (error) {
+      console.error("Error:", error);
+      return null;
+    }
   }
 }
