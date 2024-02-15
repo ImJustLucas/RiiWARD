dc1ee760b4ef4cc365eb87e1d169392977c76925
RzWilliam <williamrodriguez0621@gmail.com>
2024-02-15 09:51:12 +0100

    🩹 Remove button when logged

--- src/components/Pages/Homepage/HeroContainer.tsx
+++ src/components/Pages/Homepage/HeroContainer.tsx
@@ -1,8 +1,11 @@
 import { DefaultButton } from "@components/Common/Buttons/DefaultButton";
+import { useAuth } from "@contexts/AuthContext";
 import { H1Container, Subtitle } from "@styles/typos";
 import styled from "styled-components";
 
 export const Hero: React.FC = () => {
+  const { isLogged } = useAuth();
+
   return (
     <HeroContainer>
       <H1Container>
@@ -18,9 +21,11 @@ export const Hero: React.FC = () => {
         <DefaultButton type="primary" href="/projects">
           Découvrir tout les projets
         </DefaultButton>
-        <DefaultButton type="primary" bordered href="/auth/signup">
-          Se créer un compte
-        </DefaultButton>
+        {!isLogged && (
+          <DefaultButton type="primary" bordered href="/auth/signup">
+            Se créer un compte
+          </DefaultButton>
+        )}
       </ActionContainer>
     </HeroContainer>
   );
